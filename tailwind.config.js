e2679a35b9e2d9089430786adae2cbc3527fb37a
Lydie dunia <dunialydie@gmail.com>
2024-03-06 10:10:51 +0200

    pojet site final2

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,6 @@ export default {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'backgoround': "url('src/assets/profile-pic(4).png')",
-        'backgoround1': "url('src/assets/profile-pic(3).png')",
-        'backgoround2': "url('src/assets/profile-pic(3).png')",
-        'backgoround3': "url('src/assets/profile-pic(3).png')",
-
-      },
       fontFamily: {
         'custom': ['Cormorant', 'sans-serif'],
         'custonone':['Kdam Thmor Pro', 'sans-serif'],
